1a3e1c33e0c86467d344e00150a09b9f98ccc680
Cam <camilajimenezr21@gmail.com>
2025-09-28 16:34:41 -0500

    Nuevo botón para el scroll

---
+++ src/components/ScrollToTopButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+export default function ScrollToTopButton() {
+  const scrollToTop = () => {
+    const hero = document.getElementById("hero");
+    if (hero) {
+      hero.scrollIntoView({ behavior: "smooth" });
+    } else {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+  };
+
+  return (
+    <button
+      onClick={scrollToTop}
+      className="fixed bottom-22 right-6 bg-indigo-500 p-3 rounded-full shadow-lg hover:scale-110 transition-transform z-50"
+      aria-label="Ir arriba"
+    >
+      <img
+        src="/icons/arrowup.svg" // 🔹 agrega este ícono en tu carpeta /public/icons
+        alt="Ir arriba"
+        className="w-8 h-8"
+      />
+    </button>
+  );
+}
